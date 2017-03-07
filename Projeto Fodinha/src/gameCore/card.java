14590d5ede4fb2118102977c4d509571b70304d9
User <User@User-PC>
2017-03-07 15:01:10 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Projeto Fodinha/src/gameCore/card.java
@@ -0,0 +1,18 @@
+package gameCore;
+
+public class card {
+	private int num;
+	private int naipe;
+	public card(int num,int naipe) {
+		this.num=num;
+		this.naipe=naipe;
+	
+	}
+	public int getNum() {
+		return num;
+	}
+	public int getNaipe() {
+		return naipe;
+	}
+
+}
